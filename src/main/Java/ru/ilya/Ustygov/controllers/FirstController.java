3d3e4966b2b35869e29c712ee8120e8c09c6a693
Ilya-IU <ustyugov-1999@bk.ru>
2025-01-15 18:43:46 +0500

    Update FirstController.java
    Tests

--- src/main/Java/ru/ilya/Ustygov/controllers/FirstController.java
+++ src/main/Java/ru/ilya/Ustygov/controllers/FirstController.java
@@ -11,7 +11,28 @@ public class FirstController {
 
 
 
+        @GetMapping("/calculatorTest")
+        public String calculator(@RequestParam("a") int a, @RequestParam("b") int b,
+        @RequestParam("action") String action, Model model) {
 
+            double result;
+
+            switch (action) {
+                case "multiplication":
+                    result = a * b;
+                    break;
+                case "division":
+                    result = a / (double) b;
+                    break;
+                default:
+                    result = 0;
+                    break;
+            }
+
+            model.addAttribute("result", result);
+
+        return "first/calculator";
+    }
         @GetMapping("/calculator")
         public String calculator(@RequestParam("a") int a, @RequestParam("b") int b,
         @RequestParam("action") String action, Model model) {
